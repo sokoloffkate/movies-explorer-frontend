89972506945055ae356679a67faee0ec293f315c
Ekaterina Sokolova <sokoloffkate@gmail.com>
2023-08-29 16:18:30 +1000

    fixed minor error

--- movies/src/components/MoreButton/MoreButton.js
+++ movies/src/components/MoreButton/MoreButton.js
@@ -3,7 +3,7 @@ import React from "react";
 function MoreButton({}) {
   return (
     <section className="moreButton">
-      <button type="button" isUserLoginclassName="moreButton__btn">
+      <button type="button" className="moreButton__btn">
         Ещё
       </button>
     </section>
